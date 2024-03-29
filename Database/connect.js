c3c8e6eae5976d1c72944086f6cc4808cf71bdc7
harshita2d <harshit.upadhyay@a2d.co.in>
2024-03-29 11:13:42 +0530

    second

--- Database/connect.js
+++ Database/connect.js
@@ -4,7 +4,7 @@ const connect=async()=>{
 
  try{
 
-    await mongoose.connect('mongodb+srv://upadhyayharshit05:harshitdb@cluster0.t76sbcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect('mongodb+srv://upadhyayharshit05:harshitdb@cluster0.f9rbupn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log("Databse connected Successfully")
 }   
  catch(err){
